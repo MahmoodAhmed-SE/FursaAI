c204aecfbc9929080a510a2266492e51ad9e7f4f
MahmoodAhmed-SE <Mahmood.Ahmed.SE@outlook.com>
2025-11-08 23:09:35 +0400

    feat: simple template for requesting llm-api the extraction of jobs.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from playwright.sync_api import sync_playwright
-import time, random, os, json
+import random, os, json
+from http import client
+
 
 default_headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
@@ -30,8 +32,10 @@ def main():
         
         page.goto(URL)
 
+        page.wait_for_selector("div.elementor-post__card")
         for postElement in page.locator("div.elementor-post__card").all():
             title = postElement.locator("div.elementor-post__text").text_content()
+            title = title.strip()
             link = postElement.locator("a.elementor-post__thumbnail__link").get_attribute("href")
             if not link:
                 continue
@@ -39,7 +43,7 @@ def main():
             new_page = browser.new_page()
             new_page.set_extra_http_headers(default_headers)
 
-            new_page.wait_for_timeout(random.uniform(1000, 3000))
+            new_page.wait_for_timeout(random.uniform(1000, 4000))
 
             new_page.goto(link)
 
@@ -47,8 +51,14 @@ def main():
                 content = new_page.locator("div.entry-content.no-share div.content-inner")
                 
                 # TODO - use the llm api to extract the jobs to json format
+                l = client.HTTPConnection('localhost:4040')
                 text_content = content.inner_html()
-                print(text_content)
+                l.request("POST", "/api/v1/extract-jobs", {
+                    content: text_content,
+                })
+                resp = l.getresponse()
+                print(resp)
+
             except Exception as e:
                 print(f"Failed to get content for {title}: {e}")
                 break
